b6169f92881c633500e5aa484b72ad6e15e4bce8
Nils Dralle <nils.91@googlemail.com>
2018-05-28 15:59:01 +0200

    Login.js content

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -6,8 +6,28 @@ import Button from '@material-ui/core/Button';
 
 const Login = (props) => {   
         return (
+            <WebFrame>
             <div className="Login-Page">
+            <TextField
+            id="email"
+            label="E-Mail Adresse"
+            className="useremail"
+            />
+            <TextField
+            id="password"
+            label="Passwort"
+            type="password"
+            className="userpass"
+            />
+            <Button variant="raised" >
+              Login
+            </Button>
+            <Button variant="raised" >
+              Passwort zurücksetzen
+            </Button>
             </div>
+            <img src={logo} className="App-logo" alt="logo" />
+            </WebFrame
         )
 }
 

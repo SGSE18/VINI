f849567455e36cd325ebaf9c61c2c57b2a654c99
Nils Dralle <nils.91@googlemail.com>
2018-05-28 15:16:04 +0200

    added imports

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../media/logo.png';
+import WebFrame from '../components';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const Login = (props) => {   
+        return (
+            <div className="Login-Page">
+            </div>
+        )
+}
+
+export default Login;

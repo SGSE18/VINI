0668e4474021fa8c8e572f578ce5c6f4596fd9e9
Nils Dralle <nils.91@googlemail.com>
2018-06-04 16:19:55 +0200

    added basic form

--- frontend/src/components/ManageUserForm/ManageUserForm.js
+++ frontend/src/components/ManageUserForm/ManageUserForm.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import './ManageUserForm.css';
+import { TextField } from '@material-ui/core';
 
 const ManageUserForm = (props) => {
     return (
         <div className="Home-Page">
-            Hallo Welt!!
+            <TextField
+                id="lastname"
+                label="Nachname" />
+            <TextField
+                id="firstname"
+                label="Vorname" />
+            <TextField
+                id="company"
+                label="Unternehmen" />
+            <TextField
+                id="email"
+                label="E-Mail" />
+            <TextField
+                id="authoritylevel"
+                label="Authoritätslevel"
+            />
+            {/* // DropDownMenu would be better, but for some reason its not working */}
+            <TextField id="password"
+                label="Passwort"
+                type='password' />
+            <Button id="save"
+            label="Speichern"/>
         </div>
     )
 }

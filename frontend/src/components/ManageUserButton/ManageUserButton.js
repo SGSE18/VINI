94dd8701ff61a00cf947ecb10ff343c743bc6d1f
Nils Dralle <nils.91@googlemail.com>
2018-06-04 15:11:33 +0200

    ManageUserButton now not a class

--- frontend/src/components/ManageUserButton/ManageUserButton.js
+++ frontend/src/components/ManageUserButton/ManageUserButton.js
@@ -2,29 +2,12 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import './ManageUserButton.css';
 
-class ManageUserButton extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            isUserManagmentOpen : true,
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick() {
-    }
-    render() {
-        return (
-            <div style={{ width: '100%', display: 'inline' }}>               
-                <Button
-                    variant="raised"
-                    style={{ marginLeft: '10px', height: '20px' }}
-                    onClick={this.handleClick}
-                >
-                    Benutzer hinzufügenn/ändern
-                </Button>
-            </div>
-        )
-    }
+const ManageUserButton = (props) => {
+    return (
+        <div className="Home-Page">
+            Hallo Welt!!
+        </div>
+    )
 }
 
 export default ManageUserButton;

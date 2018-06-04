3319549f8c05e27fd1297a67a6fb580ff6a76e2c
Patrick <patrick.vogt@fh-bielefeld.de>
2018-06-04 14:41:25 +0200

    remove BrowserRouter

--- frontend/src/components/ManageUserButton/ManageUserButton.test.js
+++ frontend/src/components/ManageUserButton/ManageUserButton.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
 import { ManageUserButton } from '..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<BrowserRouter><ManageUserButton /></BrowserRouter>, div);
+  ReactDOM.render(<ManageUserButton />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

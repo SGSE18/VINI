63e22733e4a111d792011ffc311121ff0b12c0bc
Patrick <patrick.vogt@fh-bielefeld.de>
2018-06-07 22:48:18 +0200

    add WebFrame unit tests

--- frontend/src/components/WebFrame/WebFrame.test.js
+++ frontend/src/components/WebFrame/WebFrame.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { LoginButton } from '../';
 import WebFrame from './WebFrame';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BrowserRouter><WebFrame /></BrowserRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('WebFrame', () => {
+  configure({ adapter: new Adapter() });
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<WebFrame />);
+  })
+
+  it.skip('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<WebFrame> </WebFrame>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('contains 1 LoginButton', () => {
+    expect(wrapper.find(LoginButton).length).toBe(1);
+  });
 });
